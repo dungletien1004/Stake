c3d4efe92cb6ece2b627298998b9fff9529ba563
dungletien1305 <dung.letien1305@gmail.com>
2022-12-09 16:22:32 +0700

    test

--- test/test.js
+++ test/test.js
@@ -15,6 +15,7 @@ contract('Staking', async function(accounts){
   console.log(accounts);
   provider = web3.currentProvider;
   let defaultOptions = { from: accounts[0] };
+  let accountsOptions1 = { from: accounts[1] };
   let BN = web3.utils.BN;
   async function getTime() {
     return (await web3.eth.getBlock((await web3.eth.getBlockNumber())))["timestamp"];
@@ -30,25 +31,32 @@ contract('Staking', async function(accounts){
     await stakeToken.approve(staker.address, defaultAmount, defaultOptions);
     await stakeToken.transfer(staker.address, defaultAmount, defaultOptions);
 
+
+    let defaultAmountForAccount1 = web3.utils.toWei("400");
+    await stakeToken.approve(accounts[1], defaultAmountForAccount1, defaultOptions);
+    await stakeToken.transfer(accounts[1], defaultAmountForAccount1, defaultOptions);
+
     const owner = await staker.owner.call();
     let depositAmount = web3.utils.toWei("200");
     const lockedStaking = await staker.lockedStake({from: owner});
     console.log('Locked', lockedStaking)
     const unLockedStaking = await staker.unLockedStake({from: owner});
     console.log('Unlocked', unLockedStaking);
-    await stakeToken.approve(staker.address, depositAmount, defaultOptions);
-    await staker.deposit(depositAmount, apy, defaultOptions);
+    await stakeToken.approve(staker.address, depositAmount, accountsOptions1);
+    const deposit = await staker.deposit(depositAmount, apy, accountsOptions1);
+    console.log('deposit', deposit.logs[0]);
+
     let startingTime = await getTime();
     console.log('time',startingTime)
     let secsToAdvance = 60*60*24*50;
     await timeMachine.advanceTimeAndBlock(secsToAdvance);
     let nowtime = await getTime();
     console.log('time',nowtime)
-    const contract = await staker.getStakerFromId(accounts[0], 0);
+    const contract = await staker.getStakerFromId(accounts[1], 0);
     console.log('contract', contract);
-    const rewardNow = await staker.getRewardOfStakerWithId(accounts[0], 0);
+    const rewardNow = await staker.getRewardOfStakerWithId(accounts[1], 0);
     console.log('reward', Number(rewardNow));
-    const withdraw = await staker.withdraw(0,defaultOptions);
+    const withdraw = await staker.withdraw(0,accountsOptions1);
     console.log('draw', withdraw.logs[0]);
     
   })
